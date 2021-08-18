51b34f6cef46e603b0cf77ef54c2cfb3e8b264ab
sjwhole <bluemoon2718@naver.com>
2021-08-18 15:47:25 +0900

    feat: Add project schema setting

---
+++ schema.js
@@ -0,0 +1,14 @@
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
+import { mergeSchemas } from "@graphql-tools/schema";
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries,mutations}.js`
+);
+const typeDefs = mergeTypeDefs(loadedTypes);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = mergeSchemas({ typeDefs, resolvers });
+
+export default schema;
