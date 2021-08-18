307669f39cd5819bf85004fdff23d18cac58e488
sjwhole <bluemoon2718@naver.com>
2021-08-18 15:45:57 +0900

    feat: Add create account

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,37 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      // check if username or email are already on DB
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [{ username }, { email }],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/email is already taken.");
+        }
+        // hash password
+        const uglyPassword = await bcrypt.hash(password, 10);
+        // save and return the user
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
+    },
+  },
+};
