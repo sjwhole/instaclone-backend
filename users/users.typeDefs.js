1932fb69a52ebed03dcc07e6bcd5a65b2a039c42
sjwhole <bluemoon2718@naver.com>
2021-08-18 15:45:27 +0900

    feat: Add user type defs

---
+++ users/users.typeDefs.js
@@ -0,0 +1,14 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String
+    username: String!
+    email: String!
+    password: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
