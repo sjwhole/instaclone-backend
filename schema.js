e3717bf9bedb22cfd318ea0a5b47a74bab78245d
sjwhole <bluemoon2718@naver.com>
2021-08-19 15:01:44 +0900

    feta: Add resolvers schema

--- schema.js
+++ schema.js
@@ -4,7 +4,7 @@ import { mergeSchemas } from "@graphql-tools/schema";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries,mutations}.js`
+  `${__dirname}/**/*.{resolvers,queries,mutations}.js`
 );
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
