9d8b8d5de07406465002923eb41fb12862bd9ff6
sjwhole <bluemoon2718@naver.com>
2021-08-19 15:02:14 +0900

    feat: Add context logged in user

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
+import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
 const server = new ApolloServer({
   schema,
+  context: async ({ req }) => {
+    let user = await getUser(req.headers.token);
+    return {
+      loggedInUser: user,
+    };
+  },
 });
 
 server
