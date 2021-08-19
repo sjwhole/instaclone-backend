55c7b94eca1c699e6b44304cca247af8f67df3ca
sjwhole <bluemoon2718@naver.com>
2021-08-19 15:03:01 +0900

    feat: Add edit profile with protected resolver

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,40 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+import { protectedResolver } from "../users.utils";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword },
+        { loggedInUser }
+      ) => {
+        protectResolver(loggedInUser);
+        let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: { id: loggedInUser.id },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(uglyPassword && { password: uglyPassword }),
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+          };
+        }
+      }
+    ),
   },
 };
