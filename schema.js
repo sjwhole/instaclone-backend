5639d66d081154a9b8b76a14b530803920e73f2a
sjwhole <bluemoon2718@naver.com>
2021-08-20 21:19:51 +0900

    feat: Change schema to use apollo server

--- schema.js
+++ schema.js
@@ -1,14 +1,9 @@
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
-import { mergeSchemas } from "@graphql-tools/schema";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(
   `${__dirname}/**/*.{resolvers,queries,mutations}.js`
 );
-const typeDefs = mergeTypeDefs(loadedTypes);
-const resolvers = mergeResolvers(loadedResolvers);
-
-const schema = mergeSchemas({ typeDefs, resolvers });
-
-export default schema;
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);
