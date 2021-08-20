4ebac947ffefb4003489887cbe704ffa8c9c7233
sjwhole <bluemoon2718@naver.com>
2021-08-20 21:20:07 +0900

    feat: Add apollo to express middleware

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
-import schema from "./schema";
+import express from "express";
+import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
-const server = new ApolloServer({
-  schema,
+const apollo = new ApolloServer({
+  resolvers,
+  typeDefs,
   context: async ({ req }) => {
     let user = await getUser(req.headers.token);
     return {
@@ -14,6 +17,10 @@ const server = new ApolloServer({
   },
 });
 
-server
-  .listen(PORT)
-  .then(() => console.log(`Server is running on http://localhost:${PORT}/`));
+const app = express();
+app.use(logger("tiny"));
+app.use("/static", express.static("uploads"));
+apollo.applyMiddleware({ app });
+app.listen({ port: PORT }, () =>
+  console.log(`Server is running on http://localhost:${PORT}/`)
+);
