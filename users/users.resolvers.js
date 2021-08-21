6a52f0036249e37fc95c0d0a3896de8a2f5f355e
sjwhole <bluemoon2718@naver.com>
2021-08-21 13:43:59 +0900

    feat: Add user following state & isme resolver

---
+++ users/users.resolvers.js
@@ -0,0 +1,26 @@
+import client from "../client";
+
+export default {
+  User: {
+    totalFollowing: ({ id }) =>
+      client.user.count({ where: { followers: { some: { id } } } }),
+    totalFollowers: ({ id }) =>
+      client.user.count({ where: { following: { some: { id } } } }),
+    isMe: ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      return id === loggedInUser.id;
+    },
+    isFollowing: async ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      const exists = await client.user.count({
+        where: { id: loggedInUser.id, following: { some: { id } } },
+      });
+
+      return exists !== 0;
+    },
+  },
+};
