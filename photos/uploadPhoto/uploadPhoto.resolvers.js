537f20053998f7fba07ae0c8a12ebaef4715b324
sjwhole <bluemoon2718@naver.com>
2021-08-22 16:47:29 +0900

    feat: Add upload photo

---
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -0,0 +1,35 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Mutation: {
+    uploadPhoto: protectedResolver(
+      async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagsObj = null;
+        if (caption) {
+          const hashtags = caption.match(/#[\w]+/g);
+          hashtagsObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
+        }
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagsObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagsObj,
+              },
+            }),
+          },
+        });
+      }
+    ),
+  },
+};
