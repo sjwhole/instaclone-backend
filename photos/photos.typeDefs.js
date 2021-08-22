8c8fafb56c0625c5b445deaef4d2564024e8aa80
sjwhole <bluemoon2718@naver.com>
2021-08-22 16:46:36 +0900

    feat: Add photo typeDefs

---
+++ photos/photos.typeDefs.js
@@ -0,0 +1,21 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type Photo {
+    id: Int!
+    user: User!
+    file: String!
+    caption: String
+    hashtags: [Hashtag]
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Hashtag {
+    id: Int!
+    hashtag: String!
+    photos(page: Int!): [Photo]
+    totalPhotos: Int!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
