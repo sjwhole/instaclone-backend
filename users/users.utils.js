5cbe1f54e121b5690a803259952a060f0fa5f7e8
sjwhole <bluemoon2718@naver.com>
2021-08-23 18:40:36 +0900

    feat: Add operation specify in protecd resolver

--- users/users.utils.js
+++ users/users.utils.js
@@ -21,10 +21,15 @@ export const getUser = async (token) => {
 export const protectedResolver =
   (ourResolver) => (root, args, context, info) => {
     if (!context.loggedInUser) {
-      return {
-        ok: false,
-        error: "Please log in to perform this action.",
-      };
+      const query = info.operation.operation === "query";
+      if (query) {
+        return null;
+      } else {
+        return {
+          ok: false,
+          error: "Please log in to perform this action.",
+        };
+      }
     }
     return ourResolver(root, args, context, info);
   };
