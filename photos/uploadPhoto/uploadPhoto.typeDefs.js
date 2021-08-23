e1649b2f8b87ecb4423c5b03b09bbdb859ead186
sjwhole <bluemoon2718@naver.com>
2021-08-23 18:42:52 +0900

    feat: Add upload to s3 in muation

--- photos/uploadPhoto/uploadPhoto.typeDefs.js
+++ photos/uploadPhoto/uploadPhoto.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Mutation {
-    uploadPhoto(file: String!, caption: String): Photo
+    uploadPhoto(file: Upload!, caption: String): Photo
   }
 `;
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,6 +2,7 @@ import { createWriteStream } from "fs";
 import client from "../../client";
 import bcrypt from "bcrypt";
 import { protectedResolver } from "../users.utils";
+import { uploadToS3 } from "../../shared/shared.utils";
 
 const resolverFn = async (
   _,
@@ -9,15 +10,9 @@ const resolverFn = async (
   { loggedInUser }
 ) => {
   let avatarUrl = null;
+
   if (avatar) {
-    const { filename, createReadStream } = await avatar;
-    const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
-    const readStream = createReadStream();
-    const writeStream = createWriteStream(
-      process.cwd() + "/uploads/" + newFilename
-    );
-    readStream.pipe(writeStream);
-    avatarUrl = `http://localhost:4000/static/${newFilename}`;
+    avatarUrl = await uploadToS3(avatar, loggedInUser.id, "avatars");
   }
 
   let uglyPassword = null;
