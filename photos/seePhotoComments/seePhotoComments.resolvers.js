b56653dd312590c191289a49368292c0920bf267
sjwhole <bluemoon2718@naver.com>
2021-08-23 18:44:38 +0900

    feat: Add see photo comments

---
+++ photos/seePhotoComments/seePhotoComments.resolvers.js
@@ -0,0 +1,11 @@
+import client from "../../client";
+
+export default {
+  Query: {
+    seePhotoComments: (_, { id }) =>
+      client.comment.findMany({
+        where: { photoId: id },
+        orderBy: { createdAt: "asc" },
+      }),
+  },
+};
