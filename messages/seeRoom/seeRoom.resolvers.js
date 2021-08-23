24dbc89ecb0d1cd4171fd3d12e74047ed527e8c1
sjwhole <bluemoon2718@naver.com>
2021-08-23 18:46:14 +0900

    feat: Add see room

---
+++ messages/seeRoom/seeRoom.resolvers.js
@@ -0,0 +1,16 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeRoom: protectedResolver(async (_, { id }, { loggedInUser }) =>
+      client.room.findFirst({
+        where: { id, users: { some: { id: loggedInUser.id } } },
+        include: {
+          users: true,
+          messages: true,
+        },
+      })
+    ),
+  },
+};
