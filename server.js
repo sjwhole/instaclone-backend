65ba6d31f1acdd7ab8e68b40e654750b236badf6
sjwhole <bluemoon2718@naver.com>
2021-08-24 22:44:35 +0900

    feat: Add ws context

--- server.js
+++ server.js
@@ -4,16 +4,35 @@ import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
+import http from "http";
 
 const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
-  context: async ({ req }) => {
-    let user = await getUser(req.headers.token);
-    return {
-      loggedInUser: user,
-    };
+  context: async (ctx) => {
+    if (ctx.req) {
+      let user = await getUser(ctx.req.headers.token);
+      return {
+        loggedInUser: user,
+      };
+    } else {
+      const {
+        connection: { context },
+      } = ctx;
+      return {
+        loggedInUser: context.loggedInUser,
+      };
+    }
+  },
+  subscriptions: {
+    onConnect: async ({ token }) => {
+      if (!token) {
+        throw new Error("You can't listen.");
+      }
+      const loggedInUser = await getUser(token);
+      return { loggedInUser };
+    },
   },
 });
 
@@ -21,6 +40,10 @@ const app = express();
 app.use(logger("tiny"));
 app.use("/static", express.static("uploads"));
 apollo.applyMiddleware({ app });
-app.listen({ port: PORT }, () =>
+
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}/`)
 );
