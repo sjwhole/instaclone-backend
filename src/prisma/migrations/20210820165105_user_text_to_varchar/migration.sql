4481a3526a05095127b3608dc6e2295948fe36eb
sjwhole <bluemoon2718@naver.com>
2021-08-24 22:50:15 +0900

    refactor: Move file to src directory

---
+++ src/prisma/migrations/20210820165105_user_text_to_varchar/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `firstName` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(20)`.
+  - You are about to alter the column `lastName` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(20)`.
+  - You are about to alter the column `username` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(20)`.
+  - You are about to alter the column `email` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(100)`.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatar" TEXT,
+ADD COLUMN     "bio" TEXT,
+ALTER COLUMN "firstName" SET DATA TYPE VARCHAR(20),
+ALTER COLUMN "lastName" SET DATA TYPE VARCHAR(20),
+ALTER COLUMN "username" SET DATA TYPE VARCHAR(20),
+ALTER COLUMN "email" SET DATA TYPE VARCHAR(100);
+
+-- CreateTable
+CREATE TABLE "_FollowRelation" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_FollowRelation_AB_unique" ON "_FollowRelation"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_FollowRelation_B_index" ON "_FollowRelation"("B");
+
+-- AddForeignKey
+ALTER TABLE "_FollowRelation" ADD FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_FollowRelation" ADD FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
