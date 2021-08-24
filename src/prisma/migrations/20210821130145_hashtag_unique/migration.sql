afd470ae5f9580e4757fcdf77bf93aa87e2cb1d1
sjwhole <bluemoon2718@naver.com>
2021-08-24 22:52:47 +0900

    refactor: Move primsa directory

--- src/prisma/migrations/20210821130145_hashtag_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[hashtag]` on the table `Hashtag` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Hashtag.hashtag_unique" ON "Hashtag"("hashtag");
--- src/prisma/migrations/20210822123546_like_unique_together/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[photoId,userId]` on the table `Like` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Like.photoId_userId_unique" ON "Like"("photoId", "userId");
