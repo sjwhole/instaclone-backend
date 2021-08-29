d03abb9ae0e5f567f94ef9cbccb1b4effdf49f9b
sjwhole <bluemoon2718@naver.com>
2021-08-29 16:12:42 +0900

    feat: Add user me

---
+++ src/users/me/me.resolvers.js
@@ -0,0 +1,10 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Query: {
+    me: protectedResolver((_, __, { loggedInUser }) =>
+      client.user.findUnique({ where: { id: loggedInUser.id } })
+    ),
+  },
+};
